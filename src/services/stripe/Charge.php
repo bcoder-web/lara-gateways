82ed4d3604721216275609bae854fbaceed8a9a9
Sajol Mahmud <sajolmahmud010@gmail.com>
2023-01-14 00:36:05 +0600

    stripe charge update, capture, retirve added

--- src/services/stripe/Charge.php
+++ src/services/stripe/Charge.php
@@ -2,6 +2,7 @@
 
 namespace Betacoders\Gateway\services\stripe;
 
+use Stripe\Exception\ApiErrorException;
 use Stripe\Exception\CardException;
 
 class Charge extends Stripe
@@ -57,7 +58,70 @@ class Charge extends Stripe
         }
     }
 
-    public function update(array $data = []): \Stripe\ApiResponse|\Stripe\ErrorObject|null{
+    /**
+     * @param string $id
+     * @param array $data
+     * @return \Stripe\ApiResponse|\Stripe\ErrorObject|null
+     * @throws ApiErrorException
+     */
+    public function update(string $id, array $data = []): \Stripe\ApiResponse|\Stripe\ErrorObject|null{
+        try {
+            $chargeData = [];
+
+            if (!empty($this->customer)){
+                $chargeData['customer'] = $this->customer;
+            }
+
+            if (!empty($data['shipping']) && !empty($data['shipping']['address']) && !empty($data['shipping']['name'])){
+                $chargeData['shipping'] = $data['shipping'];
+            }
+            if (!empty($data['metadata'])){
+                $chargeData['metadata'] = $data['metadata'];
+            }
+            if (!empty($chargeData)){
+                $response = $this->stripe->charges->update($id, $chargeData);
+                return $response->jsonSerialize();
+            }
+
+        }catch (CardException $cardException){
+            return $cardException->getError();
+        }
+    }
+
+    /**
+     * @param string $id
+     * @return array|mixed|string
+     */
+    public function retrive(string $id): mixed
+    {
+        try {
+            $charge = $this->stripe->charges->retrieve($id);
+            return $charge->jsonSerialize();
+        } catch (ApiErrorException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * @param string $id
+     * @param array $data
+     * @return string|void
+     */
+    public function capture(string $id, array $data=[]){
+        try {
+            $captureData = [];
+            if ($data['amount']){
+                $captureData['amount'] = $data['amount'];
+            }
+            if ($data['receipt_email']){
+                $captureData['receipt_email'] = $data['receipt_email'];
+            }
+            $charge = $this->stripe->charges->retrieve($id);
+            $charge->capture($captureData);
+        }
+        catch (ApiErrorException $e) {
+            return $e->getMessage();
+        }
 
     }
 }
